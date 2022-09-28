35e1c5f981be4c7576354721b0282fe6025fd481
Fernando0657 <97993444+Fernando0657@users.noreply.github.com>
2022-09-27 19:29:56 -0500

    Delete PS2P2 (2).py

--- CIS 106/PS2P2 (2).py
+++
@@ -1,7 +0,0 @@
-lname = input("Enter your last name")
-hours = float(input("Enter enter hours worked"))
-rate = float(input("Enter your hourly rate"))
-
-gp = hours*rate 
-
-print("Your gross pay is $" ,gp)
