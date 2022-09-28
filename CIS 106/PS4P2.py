0974f969bf669ac3787b68f993def2f2bc1cc957
Fernando0657 <97993444+Fernando0657@users.noreply.github.com>
2022-09-28 15:16:45 -0500

    Add files via upload

---
+++ CIS 106/PS4P2.py
@@ -0,0 +1,6 @@
+#Calculate tuition + fee 
+lname = input("Enter your name")
+credits = float(input("How many credits did you take"))
+
+tuition = credits*250+100 
+print("Your tuiton is" , tuition) 
