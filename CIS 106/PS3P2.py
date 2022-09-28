afe3e6e49f1faa0d030936429eeb4d4a4574bf3f
Fernando0657 <97993444+Fernando0657@users.noreply.github.com>
2022-09-28 14:11:39 -0500

    Add files via upload

---
+++ CIS 106/PS3P2.py
@@ -0,0 +1,9 @@
+#Area & Perimeter 
+length = float(input("Please enter the length "))
+width = float(input("Enter the width "))
+
+area = length*width
+per = 2*(length+width)
+
+print("The area is" , area) 
+print("The perimeter is" , per)
