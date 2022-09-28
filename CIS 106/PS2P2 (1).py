5d83cc58e4ee95b4e3231d4403a6c0238e95c356
Fernando0657 <97993444+Fernando0657@users.noreply.github.com>
2022-09-27 19:05:16 -0500

    Add files via upload

---
+++ CIS 106/PS2P2 (1).py
@@ -0,0 +1,6 @@
+#Extended price 
+qty = float(input("Please enter quantity ordered"))
+ppu = float(input("Please enter price per unit"))
+ 
+extprice = qty*ppu
+print("The extended price is $", extprice) 
